5e557e941f4ec6c8a7b09e1693cf0b03bf020db0
Ishan Patnaik <155337337+ipatnaik07@users.noreply.github.com>
2026-03-12 22:34:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("attendanceButton");
+
+button.addEventListener("click", () => {
+  window.location.href = "https://youtu.be/Uc46heewnjc?si=I_jb-vF-6959BDyB";
+});
