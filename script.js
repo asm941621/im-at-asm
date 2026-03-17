bdf51f0782172e5f1a4dc76da6966fd64bb63f0a
Ishan Patnaik <155337337+ipatnaik07@users.noreply.github.com>
2026-03-17 09:55:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("attendanceButton");
 
 button.addEventListener("click", () => {
-  window.location.href = "https://app.boardingware.com/nfc/tag?v=5&u=04E3C43A561490&c=000494";
+  window.location.href = "https://app.boardingware.com/nfc/tag?v=5&u=04E3C43A561490&c=000578";
 });
