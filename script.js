2e9f99364e1a28e7f06904bb24ce1ac602ff1246
davitiem <emmett.w.davitian@gmail.com>
2026-03-17 00:38:54 -0700

    Change button redirect URL to Boardingware app

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("attendanceButton");
 
 button.addEventListener("click", () => {
-  window.location.href = "https://youtu.be/Uc46heewnjc?si=I_jb-vF-6959BDyB";
+  window.location.href = "https://app.boardingware.com/nfc/tag?v=5&u=04E3C43A561490&c=000494";
 });
